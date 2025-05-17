dbd4574c5421c8d9ccbcf207e938343e5108ee5e
chander3012 <chanderprakash3012@gmail.com>
2025-05-17 13:53:29 +0530

    Fix asset paths for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Ice-Faucet/', // 👈 required for GitHub Pages
+  base: '/Ice-Faucet/', // 👈 very important!
   plugins: [react()],
   server: {
     proxy: {
